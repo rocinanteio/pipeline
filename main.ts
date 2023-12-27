d4e32317748c66c38ec8350a2aabf642c0fc542c
Ibrahim Dagdelen <ibrahim.dagdelen@trendyol.com>
2023-12-27 23:05:20 +0300

    roci-1 add project start ep

--- main.ts
+++ main.ts
@@ -1,12 +1,51 @@
 import { io } from "npm:socket.io-client@4.7.2";
+import { processFlags } from "https://deno.land/x/flags_usage/mod.ts";
+import axiod from "https://deno.land/x/axiod/mod.ts";
 
-const socket = io("ws://localhost:4002");
+const options = {
+  description: {
+    api: "rocinante core service url",
+    socket: "rocinante core wss url",
+    name: "name of project",
+    version: "version of project",
+  },
+  argument: {
+    api: "dir",
+    socket: "dir",
+    name: "dir",
+    version: "dir",
+  },
+  string: ["api", "socket", "name", "version"],
+};
 
-socket.on("connect", () => {
-  console.log("Connected to Rocinance Core Service");
+const { api, socket, name, version } = processFlags(Deno.args, options);
+const socketConn = io(socket);
 
-  socket.on("PROJECT_START", (data: any) => {
+enum TopicNames {
+  PROJECT_START = "PROJECT_START",
+}
+
+if (!name || !version || !socket) {
+  console.error("some arguments missing!! please check you're arguments.");
+  Deno.exit(1);
+}
+
+socketConn.on("connect", () => {
+  axiod.post(`${api}/projects/start`, {
+    name,
+    version,
+  })
+    .then((response) => {
+      //TODO Set env with returned host
+      console.log(response);
+      Deno.exit(1)
+    })
+    .catch((error) => {
+      console.error(error.response.data);
+      Deno.exit(1)
+    });
+
+  socketConn.on(TopicNames.PROJECT_START, (data: string) => {
+    console.log("::: ", data);
   });
-
-  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
 });
